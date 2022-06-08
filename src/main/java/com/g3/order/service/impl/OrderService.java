5c0d95c83407711af2ef93916a520ab022c54096
lisdrl <lisandre.darioli@gmail.com>
2022-06-08 13:14:20 -0300

    feat(back): creation of OrderService

--- src/main/java/com/g3/order/service/impl/OrderService.java
+++ src/main/java/com/g3/order/service/impl/OrderService.java
@@ -1,29 +1,41 @@
 package com.g3.order.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.g3.order.controller.dto.OrderDTO;
 import com.g3.order.controller.form.OrderForm;
 import com.g3.order.model.Order;
+import com.g3.order.repository.OrderRepository;
 import com.g3.order.service.interfaces.IOrderService;
 
 @Service
 @Component
 public class OrderService implements IOrderService {
 
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
-	public List<Order> getAllOrders() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<OrderDTO> getAllOrders() {
+		List<Order> orders = orderRepository.findAll();
+		List<OrderDTO> ordersDTO = new ArrayList<>();
+		for (Order o : orders) {
+			OrderDTO orderDTO = new OrderDTO(o);
+			ordersDTO.add(orderDTO);
+		}
+		return ordersDTO;
 	}
 
 	@Override
-	public OrderDTO addNewOrder(OrderForm orderForm) {
-		// TODO Auto-generated method stub
-		return null;
+	public OrderDTO createOrder(OrderForm orderForm) {
+		Order order = orderRepository.save(orderForm.toOrder());
+		OrderDTO orderDTO = new OrderDTO(order);
+		return orderDTO;
 	}
 
 }
