fdf2929a773cad0490ce25573f53f977f35258f3
lisdrl <lisandre.darioli@gmail.com>
2022-06-08 14:53:05 -0300

    feat(back): refactoring controller due to exceptions

--- src/main/java/com/g3/order/controller/OrderController.java
+++ src/main/java/com/g3/order/controller/OrderController.java
@@ -3,9 +3,12 @@ package com.g3.order.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +22,7 @@ import com.g3.order.service.interfaces.IOrderService;
 @RestController
 @RequestMapping("/order")
 public class OrderController {
-	
+
 	@Autowired
 	private IOrderService orderService;
 
@@ -28,9 +31,16 @@ public class OrderController {
 		List<OrderDTO> ordersDTOs = orderService.getAllOrders();
 		return ResponseEntity.ok(ordersDTOs);
 	}
-	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<OrderDTO> getOrderById(@PathVariable Long id) {
+		OrderDTO orderDTO = orderService.getOrderById(id);
+		return ResponseEntity.ok(orderDTO);
+	}
+
 	@PostMapping
-	public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderForm orderForm, UriComponentsBuilder uriBuilder){
+	public ResponseEntity<OrderDTO> createOrder(@RequestBody @Valid OrderForm orderForm,
+			UriComponentsBuilder uriBuilder) {
 		OrderDTO orderDTO = orderService.createOrder(orderForm);
 		URI uri = uriBuilder.path("/order/{id}").buildAndExpand(orderDTO.getId()).toUri();
 		return ResponseEntity.created(uri).body(orderDTO);
