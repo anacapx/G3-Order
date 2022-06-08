e40328b93410793a2eec5d070c91c5187ab595c8
lisdrl <lisandre.darioli@gmail.com>
2022-06-08 14:54:04 -0300

    feat(back): adding bean validation

--- src/main/java/com/g3/order/controller/form/OrderForm.java
+++ src/main/java/com/g3/order/controller/form/OrderForm.java
@@ -1,11 +1,20 @@
 package com.g3.order.controller.form;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import com.g3.order.model.Order;
 
 public class OrderForm {
 
+	@NotNull @DecimalMin(value = "1")
 	private Long userId;
+	
+	@NotNull @DecimalMin(value = "0")
 	private Double value;
+	
+	@NotNull @NotEmpty
 	private String products;
 	
 	public OrderForm(Long userId, Double value, String products) {
