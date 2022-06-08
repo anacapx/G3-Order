d42b3c20f4f41a214624556701b0676bcc6134d2
lisdrl <lisandre.darioli@gmail.com>
2022-06-08 12:59:40 -0300

    feat(back): updates OrderForm, inserting its attributes and methods

--- src/main/java/com/g3/order/controller/form/OrderForm.java
+++ src/main/java/com/g3/order/controller/form/OrderForm.java
@@ -1,5 +1,21 @@
 package com.g3.order.controller.form;
 
+import com.g3.order.model.Order;
+
 public class OrderForm {
 
+	private Long userId;
+	private Double value;
+	private String products;
+	
+	public OrderForm(Long userId, Double value, String products) {
+		super();
+		this.userId = userId;
+		this.value = value;
+		this.products = products;
+	}
+
+	public Order toOrder() {
+		return new Order(this.userId, this.value, this.products);
+	}
 }
