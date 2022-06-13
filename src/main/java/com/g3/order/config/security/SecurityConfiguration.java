4ed1f53eb2ed0fd4865e40ddf6e4129953599773
lisdrl <lisandre.darioli@gmail.com>
2022-06-13 09:56:34 -0300

    feat(back): cors

--- src/main/java/com/g3/order/config/security/SecurityConfiguration.java
+++ src/main/java/com/g3/order/config/security/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.g3.order.config.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
